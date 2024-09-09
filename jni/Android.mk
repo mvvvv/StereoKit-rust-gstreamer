34a864c7cbad0dfc1d069b513c56a972fcf5e6bf
mv81 <mvidal0001@gmail.com>
2024-09-09 16:54:41 +0200

    opengl is mandatory to get typefindfunctions

--- jni/Android.mk
+++ jni/Android.mk
@@ -19,7 +19,7 @@ endif
 GSTREAMER_NDK_BUILD_PATH  := $(GSTREAMER_ROOT)/share/gst-android/ndk-build/
 #include $(GSTREAMER_NDK_BUILD_PATH)/plugins.mk
 #GSTREAMER_PLUGINS  := $(GSTREAMER_PLUGINS_CORE) $(GSTREAMER_PLUGINS_SYS) $(GSTREAMER_PLUGINS_EFFECTS) $(GSTREAMER_PLUGINS_NET) $(GSTREAMER_PLUGINS_CODECS_RESTRICTED) $(GSTREAMER_PLUGINS_PLAYBACK) $(GSTREAMER_PLUGINS_CODECS) 
-GSTREAMER_PLUGINS         := coreelements coretracers androidmedia autodetect typefindfunctions equalizer audioconvert audioresample videoconvertscale playback udp rtp videorate videoparsersbad libav soup  isomp4 matroska openh264 vpx vorbis 
+GSTREAMER_PLUGINS         := coreelements coretracers androidmedia autodetect opengl typefindfunctions equalizer audioconvert audioresample videoconvertscale playback udp rtp videorate videoparsersbad libav soup  isomp4 matroska openh264 vpx vorbis 
 G_IO_MODULES 			  := openssl
 GSTREAMER_EXTRA_DEPS      := openssl gstreamer-video-1.0
 
